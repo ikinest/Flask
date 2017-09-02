59d277a7be57d9705670710ea7cd5e775dc16754
ikinest <ikinest@gmail.com>
2017-09-02 21:35:58 +0800

    动态参数

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask # import Flask
+app = Flask(__name__) # init a Flask class by a fix parameter __name__
 
-@app.route("/")
+@app.route('/')#homepage view function
 def index():
-	return '<h1>Hello World!</h1>'
-if __name__ == "__main__":
-	app.run(debug = True)
+	return '<h1>Hello world!</h1>'
+
+@app.route("/user/<username>") #define a route to parase url
+def user(username): # user page
+	return '<h1>Hello,%s!</h1>'% username
+
+if __name__ == "__main__":# main function
+	app.run(debug = True)#recall flask default server and open debug model
