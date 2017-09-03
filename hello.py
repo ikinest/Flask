49471d5c612be66989f224b931f532442afacaf5
ikinest <ikinest@gmail.com>
2017-09-03 20:40:48 +0800

    响应

--- hello.py
+++ hello.py
@@ -1,16 +1,29 @@
+__author__="ikinest"
 from flask import Flask # import Flask
-from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
 app = Flask(__name__) # init a Flask class by a fix parameter __name__
 
 @app.route('/')#homepage view function
 def index():
-	User_Agent = request.headers.get('User-Agent')
-	return '<h1>Your browser is %s</h1>' % User_Agent 
-
+	#return "<h1>Bad Request!</h1>",400
+	#-------------	
+	response = make_response('<h1>This document carries a cookie!</h1>')
+	response.set_cookie('ikinest','100')
+	return response
+	#------------
+	#return redirect('http://www.google.com/')
+	#--------
+	
 @app.route("/user/<username>") #define a route to parase url
 def user(username): # user page
 	return '<h1>Hello,%s!</h1>'% username
 
+@app.route('/getuser/<id>')
+def get_user(id):
+	abort(404)
+	
 
 if __name__ == "__main__":# main function
 	app.run(debug = True)#recall flask default server and open debug model
